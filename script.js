842a769e65e23f3c3a1167dd235edb9a47d9a392
owias azizi <owaisazizi@jmail.com>
2024-08-27 18:26:19 +0430

    add some change

--- script.js
+++ script.js
@@ -10,13 +10,14 @@ const current1El = document.querySelector('#current--1')
 const player0El = document.querySelector('.player--0')
 const player1El = document.querySelector('.player--1')
 
-let  playing ,currentScore, scores, activePlayer;
+let  playing ,currentScore, scores, activePlayer,dice;
+
 
 let init = function () {
     //starting game
     diceEl.classList.add('hidden');
     currentScore = 0
-    scores = [0, 0]            //hold the score of both player in positoin
+    scores = [0, 0]//hold the score of both player in positoin
     activePlayer = Math.trunc(Math.random() *2);
     playing = true;
    
@@ -25,7 +26,6 @@ let init = function () {
     //related to newGame btn
     current0El.textContent = 0;
     current1El.textContent = 0;
-
     player0El.classList.remove('player--active')
     player1El.classList.remove('player--active')
     activePlayer === 0 ? player0El.classList.add('player--active') : player1El.classList.add('player--active')
@@ -40,11 +40,15 @@ function swichPlayer() {
     activePlayer = activePlayer === 0 ? 1 : 0;
     player0El.classList.toggle('player--active')
     player1El.classList.toggle('player--active')
+
+     setTimeout(function(){
+        diceEl.classList.add('hidden')
+     },750)
 }
 
 btnRol.addEventListener('click', function () {
     if (playing) {
-        const dice = Math.trunc(Math.random() * 6) + 1;
+         dice = Math.trunc(Math.random() * 6) + 1;
         diceEl.classList.remove('hidden')
         diceEl.src = `dice-${dice}.png`;
 
@@ -59,17 +63,17 @@ btnRol.addEventListener('click', function () {
 })
 
 btnHold.addEventListener('click', function () {
-    if (playing) {
+    if (playing && dice !== 1) {
         scores[activePlayer] += currentScore;
         document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
-
+        diceEl.classList.add('hidden')
+          
         if (scores[activePlayer] >= 100) {
             document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
             document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
             playing = false
             diceEl.classList.add('hidden')
             document.getElementById(`score--${activePlayer}`).textContent = 'Winner !'
-            
         }
         else {
             swichPlayer();
